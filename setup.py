98e60feddad3ed9bab117c6ea20fddc3a613a6df
rliskunov <liskunov.roma@yandex.ru>
2022-08-19 15:51:09 +0300

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Example task',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/rliskunov/simle_http_server',
+    license='MIT',
+    author='rliskunov',
+    author_email='liskunov.roma@yandex.ru',
+    description='Example task about DevOps'
+)
