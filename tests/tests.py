cd5371bb66ad5ff3aa0b817e549a5096bb9245e5
rliskunov <liskunov.roma@yandex.ru>
2022-08-19 15:50:41 +0300

    add simple app

---
+++ tests/tests.py
@@ -0,0 +1,25 @@
+from fastapi.testclient import TestClient
+from requests import Response
+
+from app.main import app
+
+client: TestClient = TestClient(app)
+
+
+def test_index() -> None:
+    """
+    Test GET method
+    """
+    response: Response = client.get("/")
+    assert response.status_code == 200
+    assert response.json()["message"] == 'Hello World'
+
+
+def test_hello() -> None:
+    """
+    Test GET method
+    """
+    name: str = "User"
+    response: Response = client.get(f"/hello/{name}")
+    assert response.status_code == 200
+    assert response.json()["message"] == f'Hello, {name}'
