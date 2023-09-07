2b85c0039e7086b52262a9835b1e0a4ec04f633c
rliskunov <liskunov.roma@yandex.ru>
2023-09-07 19:42:48 +0300

    feat: improve code

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Path
 
 app = FastAPI()
 
 
-@app.get("/")
-async def index():
+@app.get("/", response_model=dict)
+async def index() -> dict:
+    """
+    An endpoint that returns a Hello World message.
+    """
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def hello(name: str):
+@app.get("/hello/{name}", response_model=dict)
+async def hello(name: str = Path(..., title="The name to say hello")) -> dict:
+    """
+    An endpoint that takes a name as a parameter in the path and returns a welcome message.
+    """
+    if not name.isalpha():
+        message: str = "Name should only contain alphabets"
+        raise HTTPException(status_code=400, detail=message)
     return {"message": f"Hello, {name}"}
